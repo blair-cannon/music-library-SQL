792033eea66587eed42869d821c95c6f2e0e6f31
Blair Preston <blair.preston99@gmail.com>
2022-06-28 18:09:03 +0000

    create genre table and update references

--- crud.sql
+++ crud.sql
@@ -39,7 +39,9 @@ CREATE TABLE songs (
 	length int, 
 	artist varchar,
 	album varchar,
-	genre varchar
+	genre int,
+		foreign key (genre_id)
+		references genres(genres_id)
 );
 
 -- create realtionship table 
@@ -49,6 +51,11 @@ CREATE TABLE relationships (
 	created_at timestamp
 )
 
+-- create genre table
+CREATE TABLE genres (
+	genre_id serial primary key,
+	genre_name varchar
+)
 --  READ 
 
 -- added two membership types
@@ -66,24 +73,31 @@ values
 -- added playlists from both users 
 insert into playlists (user_id, title, length)
 values 
-('1', 'summer dazeee', '81'),
-('1', 'Christmas Songs', '105'),
-('2', 'throwbacks', '381'),
-('2', 'partayy', '60')
+	('1', 'summer dazeee', '81'),
+	('1', 'Christmas Songs', '105'),
+	('2', 'throwbacks', '381'),
+	('2', 'partayy', '60')
+
+-- add genres 
+insert into genres (genre_name)
+values
+	('holiday'),
+	('country'),
+	('folk rock')
 
 -- added songs to both of user 1's playlists 
-insert into songs (playlist_id, title, length, artist, album, genre)
+insert into songs (playlist_id, title, length, artist, album, genre_id)
 values 
-('2', 'Joy to the World', '3', 'Nat King Cole', 'Christmas with Nat', 'holiday'),
-('2', 'All I want for Christmas is You', '3', 'Mariah Carey', 'Merry Christmas', 'holiday'),
-('2', 'Silent Night', '3', 'Frank Sinatra', 'A Jolly Christmas From Frank Sinatra', 'holiday'),
-('1', 'Brown Eyed Girl', '3', 'Jimmy Buffett', 'One Particular Harbor', 'country'),
-('1', 'American Pie', '3', 'Don McLean', 'American Pie', 'folk rock')
+('2', 'Joy to the World', '3', 'Nat King Cole', 'Christmas with Nat', 1),
+('2', 'All I want for Christmas is You', '3', 'Mariah Carey', 'Merry Christmas', 1),
+('2', 'Silent Night', '3', 'Frank Sinatra', 'A Jolly Christmas From Frank Sinatra', 1),
+('1', 'Brown Eyed Girl', '3', 'Jimmy Buffett', 'One Particular Harbor', 2),
+('1', 'American Pie', '3', 'Don McLean', 'American Pie', 3)
 
 -- added the relationship between the two users 
 insert into relationships (user_id_follower, user_id_followed, created_at)
 values 
-('1', '2', '04/04/22')
+('1', '2')
 
 -- UPDATE
 
